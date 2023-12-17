4c2e08b9c0c5a7bd4172c7cb54baa78794db0962
tugacomum <internetsoundseasy@gmail.com>
2023-12-17 15:29:30 +0000

    teste

--- models/userModel.js
+++ models/userModel.js
@@ -6,11 +6,6 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please enter your name"],
     },
-    image: {
-      type: String,
-      required: false,
-      default: "https://wallpapers-clan.com/wp-content/uploads/2023/05/cool-pfp-02.jpg",
-    },
     email: {
       type: String,
       required: [true, "Please enter your email"],
